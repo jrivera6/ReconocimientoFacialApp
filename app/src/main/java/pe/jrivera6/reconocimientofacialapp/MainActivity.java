e5427e571f768f72c75a94fd30e87b159df43a1b
Jhonny Rivera <jhonny.riverap6@gmail.com>
2018-10-11 23:47:01 -0500

    Initial commit

---
+++ app/src/main/java/pe/jrivera6/reconocimientofacialapp/MainActivity.java
@@ -0,0 +1,13 @@
+package pe.jrivera6.reconocimientofacialapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
